48a7ef32081443f7a96247b615f60910f5119980
Septian Dharma Saputra Solichin <septiandss20@gmail.com>
2025-06-02 11:28:22 +0700

    Perbaiki logout redirect

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('halaman.urls')),
+    path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
