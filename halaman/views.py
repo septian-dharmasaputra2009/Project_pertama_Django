6a4e0cbec00974db860543fab2fc985b708e3760
Septian Dharma Saputra Solichin <septiandss20@gmail.com>
2025-06-03 07:36:08 +0700

    fix

--- halaman/views.py
+++ halaman/views.py
@@ -4,7 +4,6 @@ from .forms import ArtikelForm
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from django.shortcuts import redirect
 
 def home(request):
     query = request.GET.get('q')
